95f93ca1779ce2ab0b43bbb828a0cff3a8af6cd6
JulianMontero <jmontero@ingenioso.co>
2022-07-20 18:33:32 +0000

    [ADD] Se agrego funcionalidad en POS de elegir vendedor

--- pos_orderline_salesperson/static/src/js/models.js
+++ pos_orderline_salesperson/static/src/js/models.js
@@ -9,7 +9,7 @@ models.load_models([{
     fields: ['name', 'id', 'user_id'],
     domain: function(self){ return [['company_id', '=', self.config.company_id[0]]]; },
     loaded: function(self, employees) {
-        if (self.config.module_pos_hr) {
+        if (self.config.allow_orderline_user) {
             if (self.config.employee_salesperson_ids.length > 0) {
                 self.employees_salesperson = employees.filter(function(employee) {
                     return self.config.employee_salesperson_ids.includes(employee.id) || employee.user_id[0] === self.user.id;
